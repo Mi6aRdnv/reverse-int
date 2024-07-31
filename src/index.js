3166438dbfc5048509b9fa1783f151c910a8874a
Mi6aRdnv <95938371+Mi6aRdnv@users.noreply.github.com>
2024-07-31 15:38:39 +0200

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = 0;
+    for (let i = Math.abs(n); i > 0; i = Math.floor(i / 10)) {
+        console.log(i);
+        result = result * 10 + (i % 10);
+    }
+    return result;
+};
